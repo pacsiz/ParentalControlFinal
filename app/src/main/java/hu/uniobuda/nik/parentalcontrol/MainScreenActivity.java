1756dad2ee3f63efd4a786b9a0d34aee3577fc85
pacsiz <aikidani@gmail.com>
2015-04-05 10:10:14 +0200

    0405

---
+++ app/src/main/java/hu/uniobuda/nik/parentalcontrol/MainScreenActivity.java
@@ -0,0 +1,14 @@
+package hu.uniobuda.nik.parentalcontrol;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+
+}
